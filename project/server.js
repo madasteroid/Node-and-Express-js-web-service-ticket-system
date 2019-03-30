7ac1a34fa030fe5093aec70190dc50cd65478461
madasteroid <prakash.timalsina@selu.edu>
2019-03-29 23:23:41 -0500

    string

--- project/server.js
+++ project/server.js
@@ -2,17 +2,20 @@ var express = require('express');
 var chalk = require('chalk');
 var app = express();
 var router = express.Router();
+var port = process.env.port || 80; 
 
 router.get('/test', function(req, res) {
     res.status(200).send('Hello world');
 });
+router.get('/list', function(req, res) {
+    res.status(200).send('Get all tickets');
+});
+router.post({
+
+});
 
 app.use('/api', router);
+app.use('/test', router);
 
-app.listen(1069, function(err) {
-    if (err) {
-        console.log(chalk.red(err));
-    } else {
-        console.log(chalk.blue('Magic Happens on Port 69'));
-    }
-});
+app.listen(port, function(err) {
+        console.log("Your app is running at the port")});
