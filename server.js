729a604744d08141176e596984bd936a06344ba0
madasteroid <prakash.timalsina@selu.edu>
2019-03-30 00:32:41 -0500

    make it better

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ var app = express();
 var router = express.Router();
 var port = process.env.PORT || 80;
 
-//This is the section where it will access an endpoint such as:
-//https://desolate-atoll-59781.herokuapp.com/api/test (note /api/test/)
 router.get('/test', function(req, res) {
     res.status(200).send('Hello world');
 });
+router.get('/list', function(req, res) {
+    res.status(200).send('Get all tickets');
+});
 
 app.use('/test', router);
-app.use('/api', router);
+app.use('/rest', router);
 
 app.listen(port, function() {
     console.log("Node app is running at localhost:" + port)
