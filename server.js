8fd3990681be8d9024f5e2028a3a4b0492d57753
madasteroid <prakash.timalsina@selu.edu>
2019-03-30 00:37:36 -0500

    make it better

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ var app = express();
 var router = express.Router();
 var port = process.env.PORT || 80;
 
-router.get('/test', function(req, res) {
-    res.status(200).send('Hello world');
-});
+
 router.get('/list', function(req, res) {
-    res.status(200).send('Get all tickets');
+    res.status(200).send('Endpoint to get all tickets');
+});
+router.get('/id', function(req, res) {
+    res.status(200).send('Endpoint to get a single ticket');
+});
+router.post('/ticket', function(req, res){
+    res.status(200).send('Endpoint to create a new ticket');
 });
 
-app.use('/test', router);
+app.use('/ticket', router);
+app.use('/list', router);
+app.use('/id', router);
 app.use('/rest', router);
+
 
 app.listen(port, function() {
     console.log("Node app is running at localhost:" + port)
