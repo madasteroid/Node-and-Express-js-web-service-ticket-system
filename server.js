4e38bcf101eedfaeac6b0bd24d02cb4b7cf50136
madasteroid <prakash.timalsina@selu.edu>
2019-04-08 00:59:04 -0500

    string

--- server.js
+++ server.js
@@ -4,21 +4,52 @@ var app = express();
 var router = express.Router();
 var port = process.env.PORT || 80;
 
+const tickets = require('./tickets');
 
 router.get('/list', function(req, res) {
-    res.status(200).send('Endpoint to get all tickets');
+    res.status(200).send('List of all tickets');
 });
-router.get('/ticket/id', function(req, res) {
-    res.status(200).send('Endpoint to get a single ticket');
-});
-router.post('/ticket', function(req, res){
-    res.status(200).send('Endpoint to create a new ticket');
-});
+
+
+router.get('/ticket/:id',(req, res) =>{
+    const ticket = tickets.find(t => t.id === parseInt(req.params.id));
+    if(!ticket) res.status(404).send('Ticket not found.');
+    res.send(ticket);
+ 
+ });
+
+
+ router.post('/ticket', (req,res) => {
+    const ticket = {
+        id: tickets.length+1,
+        created_at: req.body.created_at,
+        updated_at: req.body.updated_at,
+        type: req.body.type,
+        subject: req.body.subject,
+        description: req.body.description,
+        priority: req.body.priority,
+        status: req.body.status,
+        recipient: req.body.recipient,
+        submitter: req.body.submitter
+    };
+    if( !ticket.created_at || 
+        !ticket.updated_at || 
+        !ticket.type || 
+        !ticket.subject || 
+        !ticket.description || 
+        !ticket.priority || 
+        !ticket.status || 
+        !ticket.recipient ||
+        !ticket.submitter) res.status(404).send('No complete ticket information.');
+    tickets.push(ticket);
+    res.send(ticket);
+})
 
 app.use('/ticket', router);
 app.use('/list', router);
 app.use('/id', router);
 app.use('/rest', router);
+app.use(express.json());
 
 
 app.listen(port, function() {
---
+++ ticket.js
@@ -0,0 +1,36 @@
+module.exports = tickets;
+
+let tickets = [
+    {
+       "id": 1,
+       "created_at": "04-02-2019",
+       "updated_at": "04-08-2019",
+       "type": "football",
+       "subject": "Saints Vs Eagle",
+       "description": "Historic match between Saints and Eagle after 10 years",
+       "priority": "less",
+       "status": "open",
+       "recipient": "john.burris@selu.edu",
+       "submitter": "prakash.timalsina@selu.edu",
+       "assignee_id": 5532,
+       "follower_ids": [5532, 222],
+       "tags": ["enterprise", "printers"],
+      },
+
+      {
+       "id": 2,
+       "created_at": "04-02-2019",
+       "updated_at": "04-04-2019",
+       "type": "basketball",
+       "subject": "Match between Pelicans and Kings",
+       "description": "Smoothie King Center ",
+       "priority": "high",
+       "status": "open",
+       "recipient": "john.burris@selu.edu",
+       "submitter": "prakash.timalsina@selu.edu",
+       "assignee_id": 6256,
+       "follower_ids": [6256, 324],
+       "tags": ["enterprise", "printers"],
+      }
+];
+
